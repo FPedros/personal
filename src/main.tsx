a840ef0d4db2a4d6c01ddee1122ed1b5d890d489
Fernando Sousa <fernandoppsousa@gmail.com>
2025-12-26 10:31:18 -0300

    tst

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/Proposal";
+import Home from "./pages/Home";
+import Proposal from "./pages/Proposal";
 import "./index.css";
+
+function Router() {
+  const [hash, setHash] = React.useState(window.location.hash);
+
+  React.useEffect(() => {
+    const handler = () => setHash(window.location.hash);
+    window.addEventListener("hashchange", handler);
+    return () => window.removeEventListener("hashchange", handler);
+  }, []);
+
+  const path = hash.replace(/^#/, "");
+
+  if (path.startsWith("/orcamento1")) {
+    return <Proposal />;
+  }
+
+  return <Home />;
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
